f8d3bd58f281cb6d8b841b411ec847d58e8ab143
Evgeny Ageev <evgeny.ageev@novator-it.ru>
2019-02-23 03:55:09 +0300

    Meals repository on Spring JDBC

--- src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcMealRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcMealRepositoryImpl.java
@@ -1,37 +1,86 @@
 package ru.javawebinar.topjava.repository.jdbc;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.support.DataAccessUtils;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @Repository
 public class JdbcMealRepositoryImpl implements MealRepository {
+    private static final String GET_ALL_QUERY = "SELECT * FROM meals ORDER BY datetime DESC";
+    private static final String GET_BETWEEN_QUERY = "SELECT * FROM meals " +
+            "WHERE datetime between ?::timestamp AND ?::timestamp " +
+            "ORDER BY datetime DESC";
+    private static final String GET_BY_ID_QUERY = "SELECT * FROM meals WHERE id=?";
+    private static final String DELETE_QUERY = "DELETE FROM meals WHERE id=?";
+    private static final String UPDATE_QUERY = "UPDATE meals " +
+            "SET datetime=:datetime, description=:description, calories=:calories " +
+            "WHERE id=:id";
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:ss");
+
+    private static final BeanPropertyRowMapper<Meal> ROW_MAPPER = BeanPropertyRowMapper.newInstance(Meal.class);
+
+    private final JdbcTemplate jdbcTemplate;
+
+    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
+    private final SimpleJdbcInsert insert;
+
+    @Autowired
+    public JdbcMealRepositoryImpl(JdbcTemplate jdbcTemplate, NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
+        this.insert = new SimpleJdbcInsert(jdbcTemplate)
+                .withTableName("meals")
+                .usingGeneratedKeyColumns("id");
+        this.jdbcTemplate = jdbcTemplate;
+        this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
+    }
 
     @Override
     public Meal save(Meal meal, int userId) {
-        return null;
+        MapSqlParameterSource map = new MapSqlParameterSource()
+                .addValue("id", meal.getId())
+                .addValue("datetime", meal.getDateTime())
+                .addValue("description", meal.getDescription())
+                .addValue("calories", meal.getCalories());
+
+        if (meal.isNew()) {
+            Number newKey = insert.executeAndReturnKey(map);
+            meal.setId(newKey.intValue());
+        } else if (namedParameterJdbcTemplate.update(UPDATE_QUERY, map) == 0) {
+            return null;
+        }
+        return meal;
     }
 
     @Override
     public boolean delete(int id, int userId) {
-        return false;
+        return jdbcTemplate.update(DELETE_QUERY, id) != 0;
     }
 
     @Override
     public Meal get(int id, int userId) {
-        return null;
+        List<Meal> meals = jdbcTemplate.query(GET_BY_ID_QUERY, ROW_MAPPER, id);
+        return DataAccessUtils.singleResult(meals);
     }
 
     @Override
     public List<Meal> getAll(int userId) {
-        return null;
+        return jdbcTemplate.query(GET_ALL_QUERY, ROW_MAPPER);
     }
 
     @Override
     public List<Meal> getBetween(LocalDateTime startDate, LocalDateTime endDate, int userId) {
-        return null;
+        return jdbcTemplate.query(GET_BETWEEN_QUERY, ROW_MAPPER,
+                startDate.format(formatter), endDate.format(formatter));
     }
 }
--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,5 +1,6 @@
 DELETE FROM user_roles;
 DELETE FROM users;
+DELETE FROM meals;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
 INSERT INTO users (name, email, password) VALUES
@@ -9,3 +10,16 @@ INSERT INTO users (name, email, password) VALUES
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
   ('ROLE_ADMIN', 100001);
+
+INSERT INTO meals (id, datetime, description, calories) VALUES
+  (nextval('global_seq'::regclass), '2015-05-30 10:00', 'Завтрак', 500),
+  (nextval('global_seq'::regclass), '2015-05-30 13:00', 'Обед', 1000),
+  (nextval('global_seq'::regclass), '2015-05-30 20:00', 'Ужин', 500),
+  (nextval('global_seq'::regclass), '2015-05-31 10:00', 'Завтрак', 1000),
+  (nextval('global_seq'::regclass), '2015-05-31 13:00', 'Обед', 500),
+  (nextval('global_seq'::regclass), '2015-05-31 20:00', 'Ужин', 510),
+  (nextval('global_seq'::regclass), '2015-06-12 09:00', 'Завтрак', 250),
+  (nextval('global_seq'::regclass), '2015-06-12 11:00', 'Брекфаст', 350),
+  (nextval('global_seq'::regclass), '2015-06-12 13:00', 'Обед', 500),
+  (nextval('global_seq'::regclass), '2015-06-12 17:00', 'Ланч', 250),
+  (nextval('global_seq'::regclass), '2015-06-12 19:00', 'Ужин', 500);
